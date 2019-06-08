8b33529f5f90b53780e95a79a386a907d3d09810
B1tF8er <ofeth@hotmail.com>
2019-06-07 20:42:09 -0600

    Add circular buffer solution

---
+++ circular-buffer/CircularBuffer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CircularBuffer<T>
+{
+    private readonly int maxCapacity;
+    private readonly Queue<T> queue;
+
+    public CircularBuffer(int capacity)
+    {
+        maxCapacity = capacity;
+        queue = new Queue<T>(capacity);
+    }
+
+    public T Read() => queue.Count == 0
+        ? throw new InvalidOperationException("No elements")
+        : queue.Dequeue();
+
+    public void Write(T value)
+    {
+        if (queue.Count == maxCapacity)
+            throw new InvalidOperationException("Full buffer");
+        
+        queue.Enqueue(value);
+    }
+
+    public void Overwrite(T value)
+    {
+        if (queue.Count < maxCapacity)
+            queue.Enqueue(value);
+        else
+        {
+            queue.Dequeue();
+            queue.Enqueue(value);
+        }
+    }
+
+    public void Clear() => queue.Clear();
+}
