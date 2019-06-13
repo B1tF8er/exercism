6ae2ccd1eb0dabf1fc7107e7268ac257dd2c2cf5
B1tF8er <ofeth@hotmail.com>
2019-06-12 23:34:03 -0600

    Add Sum of Multiples solution

---
+++ sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max) =>
+        Enumerable.Range(1, max - 1)
+            .Where(NumberIsMultipleOfAny(multiples))
+            .Sum();
+
+    private static Func<int, bool> NumberIsMultipleOfAny(IEnumerable<int> multiples) =>
+         number => multiples
+            .Where(multiple => multiple != 0)
+            .Any(multiple => number % multiple == 0);
+}
