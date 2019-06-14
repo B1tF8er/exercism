5f17009a41b634d1d6a79570c4f385b8acc09932
B1tF8er <ofeth@hotmail.com>
2019-06-14 14:34:29 -0600

    Add Armstrong Numbers solution

---
+++ armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+using static System.Math;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number) => number
+        .ToString()
+        .Select(digit => Convert.ToInt32(digit - 48))
+        .Sum(digit => Pow(digit, number.ToString().Length)) == number;
+}
