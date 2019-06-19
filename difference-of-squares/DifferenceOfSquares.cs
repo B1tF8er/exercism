39ddbb068f12c7951f23b974d72db2e19133e2a1
B1tF8er <ofeth@hotmail.com>
2019-06-18 22:29:11 -0600

    Add Difference of Squares Solution

---
+++ difference-of-squares/DifferenceOfSquares.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using static System.Math;
+using static System.Convert;
+
+public static class DifferenceOfSquares
+{
+    public static int CalculateSquareOfSum(int max) =>
+        ToInt32(Pow(Enumerable.Range(1, max).Sum(), 2));
+
+    public static int CalculateSumOfSquares(int max) =>
+        Enumerable.Range(1, max).Select(n => ToInt32(Pow(n, 2))).Sum();
+
+    public static int CalculateDifferenceOfSquares(int max) =>
+        CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+}
