a9677e434faa9dc4c2a15660677cbe04373e868e
B1tF8er <ofeth@hotmail.com>
2019-06-18 22:50:45 -0600

    Add Grains Solution

---
+++ grains/Grains.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using static System.Convert;
+
+public static class Grains
+{
+    public static ulong Square(int n) =>
+        (n <= 0 || n > 64)
+            ? throw new ArgumentOutOfRangeException(nameof(n))
+            : n == 1 ? 1 : 2 * Square(n - 1);
+
+    public static ulong Total() =>
+        ToUInt64(Enumerable
+            .Range(1, 64)
+            .Select(n => Square(n))
+            .Select(ToDecimal)
+            .Sum());
+}
