81bd5f6df68491838444bfbb168ff4c9b12f4aed
B1tF8er <ofeth@hotmail.com>
2019-06-18 23:07:06 -0600

    Add Pangram Solution

---
+++ pangram/Pangram.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input) =>
+        input.ToLowerInvariant()
+            .Where(c => c >= 'a' && c <= 'z')
+            .Distinct()
+            .Count() == 26;
+}
