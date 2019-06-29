d4d7692fffd6f6a608404b71f288e1f71a725045
B1tF8er <ofeth@hotmail.com>
2019-06-28 19:46:13 -0600

    Add Proverb solution

---
+++ proverb/Proverb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+public static class Proverb
+{
+    public static string[] Recite(string[] subjects) =>
+        Enumerable.Range(1, subjects.Length)
+            .Select((lineIndex) =>
+                (lineIndex == subjects.Length) 
+                ? $"And all for the want of a {subjects[0]}."
+                : $"For want of a {subjects[lineIndex - 1]} the {subjects[lineIndex]} was lost.")
+            .ToArray();
+}
