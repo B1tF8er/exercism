af66e58a62329b0cb04ec9c0d89c1332e82f5dc6
B1tF8er <ofeth@hotmail.com>
2019-06-30 17:58:14 -0600

    Add Error Handling solution

---
+++ error-handling/ErrorHandling.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public static class ErrorHandling
+{
+    public static void HandleErrorByThrowingException() =>
+        throw new Exception("Generic error.");
+
+    public static int? HandleErrorByReturningNullableType(string input)
+    {
+        if (int.TryParse(input, out var result))
+            return result;
+        
+        return null;
+    }
+
+    public static bool HandleErrorWithOutParam(string input, out int result) =>
+        int.TryParse(input, out result);
+
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
+    {
+        using (disposableObject)
+            throw new Exception("Dispose error.");
+    }
+}
