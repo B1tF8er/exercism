e544fbce2143fadad0afd51436949311c1d781ca
B1tF8er <ofeth@hotmail.com>
2019-06-30 18:42:05 -0600

    Add Perfect Numbers solution

---
+++ perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        if (number < 1)
+            throw new ArgumentOutOfRangeException(nameof(number));
+
+        var aliquotSum = number.GetAliquotSum();
+
+        if (aliquotSum == number)
+            return Classification.Perfect;
+
+        if (aliquotSum > number)
+            return Classification.Abundant;
+
+        return Classification.Deficient;
+    }
+
+    private static int GetAliquotSum(this int number) =>
+        Enumerable
+            .Range(1, number - 1)
+            .Where(n => number % n == 0)
+            .Sum();
+}
