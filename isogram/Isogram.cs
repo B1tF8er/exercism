e7e233a0cc4378d3834e81b854b1be623915f81c
B1tF8er <ofeth@hotmail.com>
2019-07-09 20:22:52 -0600

    Add Isogram solution

---
+++ isogram/Isogram.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word) =>
+        word.ToLowerInvariant()
+            .Where(char.IsLetter)
+			.GroupBy(letter => letter)
+			.All(letter => letter.Count() == 1);
+}
