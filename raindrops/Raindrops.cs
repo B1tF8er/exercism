a8a8b00875441bb1c1f1aa53eee5517839d76c8e
B1tF8er <ofeth@hotmail.com>
2019-07-13 02:53:29 -0600

    Refactor Raindrops solution

--- raindrops/Raindrops.cs
+++ raindrops/Raindrops.cs
@@ -1,19 +1,70 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using static Constants.Numbers;
+using static Constants.Sounds;
 
 public static class Raindrops
 {
-    public static string Convert(int number)
-    {
-        var sound = number.IsFactorOf(3, "Pling")
-            + number.IsFactorOf(5, "Plang")
-            + number.IsFactorOf(7, "Plong");
+    private static readonly IList<ValueTuple<int, string>> factors;
 
-        if (string.IsNullOrEmpty(sound))
-            return $"{number}";
-        else
-            return sound;
+    static Raindrops()
+    {
+        factors = new List<ValueTuple<int, string>>
+        {
+            (Three, Pling),
+            (Five, Plang),
+            (Seven, Plong)
+        };
     }
 
-    private static string IsFactorOf(this int number, int factor, string sound) =>
-        number % factor == 0 ? sound : string.Empty;
+    public static string Convert(int number)
+    {
+        var sound = number.GetSound();
+
+        return sound.IsEmpty() ? $"{number}" : sound;
+    }
+
+    private static string GetSound(this int number) =>
+        factors.Aggregate(string.Empty, GetSoundOf(number));
+
+    private static Func<string, (int, string), string> GetSoundOf(int number) =>
+        (accumulator, next) =>
+        {
+            var tuple = next.ToNamedTuple();
+            
+            return accumulator + number
+                .IsFactorOf(tuple.factor)
+                .MakeSound(tuple.sound);
+        };
+
+    private static (int factor, string sound) ToNamedTuple(this (int factor, string sound) namedTuple) =>
+        namedTuple;
+
+    private static bool IsFactorOf(this int number, int factor) =>
+        number % factor == Zero;
+
+    private static string MakeSound(this bool makeSound, string sound) =>
+        makeSound ? sound : string.Empty;
+
+    private static bool IsEmpty(this string value) =>
+        string.IsNullOrWhiteSpace(value);
+}
+
+internal static class Constants
+{
+    internal static class Numbers
+    {
+        internal const int Zero = 0;
+        internal const int Three = 3;
+        internal const int Five = 5;
+        internal const int Seven = 7;
+    }
+
+    internal static class Sounds
+    {
+        internal const string Pling = "Pling";
+        internal const string Plang = "Plang";
+        internal const string Plong = "Plong";
+    }
 }
