9b0e0192db7ed5a47ce27e81e8d2f8a8d3b3ed5f
B1tF8er <ofeth@hotmail.com>
2019-08-06 20:49:13 -0600

    Add Acronym solution

---
+++ acronym/Acronym.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public static class Acronym
+{
+    private static readonly char[] separators = new [] { ' ', '-', '_' };
+
+    public static string Abbreviate(string phrase) =>
+        string.Join(
+            string.Empty,
+            phrase
+                .Split(separators, StringSplitOptions.RemoveEmptyEntries)
+                .Where(word => word.Any(char.IsLetter))
+                .Select(word => char.ToUpper(word[0]))
+        );
+}
