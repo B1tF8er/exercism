439dbd8594075eff7a3bef0201c6b9169ead86c1
B1tF8er <ofeth@hotmail.com>
2019-09-20 20:30:56 -0600

    Add isbn verifier solution

---
+++ isbn-verifier/IsbnVerifier.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class IsbnVerifier
+{
+    public static bool IsValid(string number)
+    {
+        var isbn = number.Replace("-", string.Empty);
+        
+        return HasValidFormat(isbn)
+            ? IsValidIsbn10(isbn)
+            : false;
+    }
+
+    private static bool HasValidFormat(string isbn) =>
+        Regex.IsMatch(isbn, @"^(\d{9}[\dX])$");
+
+    private static bool IsValidIsbn10(string isbn) =>
+        isbn.Select(ToInt)
+            .Select(ToIsbn10)
+            .Sum() % 11 == 0;
+
+    private static Func<char, int> ToInt => @char =>
+    {
+        if (@char == 'X')
+            return 10;
+
+        return Convert.ToInt32(@char.ToString());
+    };
+
+    private static Func<int, int, int> ToIsbn10 =>
+        (digit, index) => digit * (10 - index);
+}
