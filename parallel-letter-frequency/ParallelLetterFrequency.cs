5dcdd7f98ee709da207e429f785dcd6e27b9a9d9
B1tF8er <ofeth@hotmail.com>
2019-09-29 19:23:21 -0600

    Update parallel letter frequency solution

--- parallel-letter-frequency/ParallelLetterFrequency.cs
+++ parallel-letter-frequency/ParallelLetterFrequency.cs
@@ -4,26 +4,10 @@ using System.Linq;
 
 public static class ParallelLetterFrequency
 {
-    public static Dictionary<char, int> Calculate(IEnumerable<string> texts)
-    {
-        var letterGroups = texts
-            .AsParallel()
-            .SelectMany(text => text
-                .ToLower()
-                .Where(char.IsLetter)
-                .GroupBy(letter => letter)
-            );
-
-        var result = new Dictionary<char, int>();
-
-        foreach (var group in letterGroups)
-        {
-            if (result.ContainsKey(group.Key))
-                result[group.Key] += group.Count();
-            else
-                result[group.Key] = group.Count();
-        }
-
-        return result;
-    }
+    public static Dictionary<char, int> Calculate(IEnumerable<string> texts) =>
+        texts.AsParallel()
+            .SelectMany(text => text.ToLower())
+            .Where(char.IsLetter)
+            .GroupBy(letter => letter)
+            .ToDictionary(group => group.Key, group => group.Count());
 }
