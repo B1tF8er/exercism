5f8eff05359fa672d612802cb7d1eba78156f3ba
Jorge Yañez <ofeth@hotmail.com>
2020-02-07 14:43:04 -0700

    Add hamming solution

---
+++ fsharp/hamming/Hamming.fs
@@ -0,0 +1,18 @@
+﻿module Hamming
+
+let toCharArray (s: string) =
+    s.ToCharArray();
+
+let different (l, r) =
+    l <> r
+    
+let getDistanceTo (strand1: string) (strand2: string) : int option =
+    toCharArray strand1 |> Array.zip <| toCharArray strand2
+    |> Array.filter different
+    |> Array.length
+    |> Some
+
+let distance (strand1: string) (strand2: string): int option =
+    if strand1.Length = strand2.Length
+    then strand1 |> getDistanceTo <| strand2
+    else None
