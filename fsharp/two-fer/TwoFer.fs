95f0f20bfaf27eef9cb0d2e44946444bf3e29dc4
Jorge Yañez <ofeth@hotmail.com>
2020-02-06 17:38:22 -0700

    Add two fer solution

---
+++ fsharp/two-fer/TwoFer.fs
@@ -0,0 +1,6 @@
+﻿module TwoFer
+
+let twoFer (input: string option): string =
+    match input with
+        | Some n -> sprintf "One for %s, one for me." n
+        | None -> sprintf "One for you, one for me."
