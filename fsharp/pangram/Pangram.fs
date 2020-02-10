1454c85eeb21a4259f120a46dadc91bf347bf614
Jorge Yañez <ofeth@hotmail.com>
2020-02-10 12:19:01 -0700

    Add pangram solution

---
+++ fsharp/pangram/Pangram.fs
@@ -0,0 +1,11 @@
+﻿module Pangram
+
+let normalize (s: string) =
+    s.ToLowerInvariant()
+
+let contains (s: string) (c: char)  =
+    let n = normalize s
+    n.Contains(c)
+
+let isPangram (input: string): bool =
+    Seq.forall (input |> contains) <| ['a'..'z']
