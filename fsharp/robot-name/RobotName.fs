73928fed0eb48e2012d420e327142954c84b82fa
Jorge Yañez <ofeth@hotmail.com>
2020-02-10 13:47:14 -0700

    Add robot name solution

---
+++ fsharp/robot-name/RobotName.fs
@@ -0,0 +1,26 @@
+﻿module RobotName
+open System
+
+type Robot = { name: string }
+
+let alphabet = ['A' .. 'Z']
+
+let random n = Random().Next(n)
+
+let getRandomLetter() =
+    alphabet.[random 26]
+
+let getRandomName() =
+    let letters = [getRandomLetter(); getRandomLetter()] |> List.map string
+    String.concat "" <| letters
+
+let getRandomNumber() =
+    let number = random 1000
+    number.ToString("000")
+
+let mkRobot() =
+    { name = getRandomName() + getRandomNumber() }
+
+let name robot = robot.name
+
+let reset robot = mkRobot()
