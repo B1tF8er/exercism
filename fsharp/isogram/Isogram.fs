7edd77146071bd94948f7bb4a6c3082d3592225a
Jorge Yañez <ofeth@hotmail.com>
2020-02-10 23:28:05 -0700

    Add isogram solution

---
+++ fsharp/isogram/Isogram.fs
@@ -0,0 +1,13 @@
+﻿module Isogram
+
+open System
+
+let onlyOne (_, values) =
+    Seq.length values = 1
+
+let isIsogram str =
+    str
+    |> Seq.filter Char.IsLetter
+    |> Seq.map Char.ToLowerInvariant
+    |> Seq.groupBy id
+    |> Seq.forall onlyOne
