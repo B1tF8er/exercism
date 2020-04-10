a88c5cfe3497578be55d605dbf955383e3629603
Jorge Yañez <ofeth@hotmail.com>
2020-04-10 15:47:43 -0600

    Refactor queen attack

--- fsharp/queen-attack/QueenAttack.fs
+++ fsharp/queen-attack/QueenAttack.fs
@@ -11,6 +11,14 @@ let isValidPosition position =
 let create (row, col) =
     row |> isValidPosition && col |> isValidPosition
 
+let sameRowAs row1 row2 =
+    row1 = row2
+
+let sameColumnAs col1 col2 =
+    col1 = col2
+
+let inRangeOf (row1, col1) (row2, col2) =
+    (row1 - row2 |> abs) = (col1 - col2 |> abs)
+
 let canAttack (row1, col1) (row2, col2) =
-    if row1 = row2 && col1 = col2 then false
-    else row1 = row2 || col1 = col2 || (row1 - row2 |> abs) = (col1 - col2 |> abs)
+    row2 |> sameRowAs row1 ||  col2 |> sameColumnAs col1 || (row2, col2) |> inRangeOf (row1, col1)
