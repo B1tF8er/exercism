b29b813623fdb66517837b9c41d1cdc94bd68e7a
Jorge Yañez <ofeth@hotmail.com>
2020-04-10 15:37:55 -0600

    Add queen attack solution

---
+++ fsharp/queen-attack/QueenAttack.fs
@@ -0,0 +1,16 @@
+﻿module QueenAttack
+
+open System
+
+let abs (x:int) =
+    Math.Abs x
+
+let isValidPosition position =
+    position >= 0 && position < 8
+
+let create (row, col) =
+    row |> isValidPosition && col |> isValidPosition
+
+let canAttack (row1, col1) (row2, col2) =
+    if row1 = row2 && col1 = col2 then false
+    else row1 = row2 || col1 = col2 || (row1 - row2 |> abs) = (col1 - col2 |> abs)
