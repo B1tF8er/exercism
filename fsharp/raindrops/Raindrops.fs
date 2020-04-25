52011c475af119446f67ff5cad028f6d4a1f7d06
Jorge Yañez <ofeth@hotmail.com>
2020-04-24 23:34:39 -0600

    Add raindrops solution

---
+++ fsharp/raindrops/Raindrops.fs
@@ -0,0 +1,21 @@
+﻿module Raindrops
+
+let private factors =
+    [(3, "Pling"); (5, "Plang"); (7, "Plong")]
+
+let private isFactorOf factor number =
+    number % factor = 0
+
+let private concatSounds soundsSoFar nextSound =
+    sprintf "%s%s" soundsSoFar nextSound
+
+let convert (number: int): string =
+    let sounds = [
+        for (factor, sound) in factors do
+            if number |> isFactorOf factor
+                then yield sound
+    ]
+
+    match sounds with
+    | [] -> sprintf "%i" number
+    | sounds -> ("", sounds) ||> List.fold concatSounds
