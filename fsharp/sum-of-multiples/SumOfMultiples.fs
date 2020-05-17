2d4efc7fe54e2c9b48ddc684f903437211ed01e5
Jorge Yañez <ofeth@hotmail.com>
2020-05-17 14:26:34 -0600

    Add sun of multiples solution

---
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,15 @@
+﻿module SumOfMultiples
+
+let isFactorOfAny numbers number  =
+    numbers |> List.exists (fun factor ->
+        match factor with
+        | 0 -> false
+        | _ -> number % factor = 0
+    )
+
+let sum (numbers: int list) (upperBound: int): int =
+    let isFactorOf = isFactorOfAny numbers
+
+    [1 .. upperBound - 1]
+    |> List.filter isFactorOf
+    |> List.sum
