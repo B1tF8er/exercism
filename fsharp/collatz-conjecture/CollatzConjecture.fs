6916ecab0c637e8fec2d5f8c21169a7c43925b7d
Jorge Yañez <ofeth@hotmail.com>
2020-05-17 20:11:08 -0600

    Add collatz conjeture solution

---
+++ fsharp/collatz-conjecture/CollatzConjecture.fs
@@ -0,0 +1,12 @@
+﻿module CollatzConjecture
+
+let rec private counter seed number =
+    match number with 
+    | n when n < 1 -> None
+    | n when n = 1 -> Some seed
+    | n when n % 2 = 0 -> counter (seed + 1) (n / 2)
+    | _ -> counter (seed + 1) (number * 3 + 1)
+
+let steps number =
+    counter 0 number
+ 
