ef9218d9e891cc5e7201283f08f3de8fae149934
Jorge Yañez <ofeth@hotmail.com>
2020-05-28 23:58:57 -0600

    Add Armstrong Numbers solution

---
+++ fsharp/armstrong-numbers/ArmstrongNumbers.fs
@@ -0,0 +1,8 @@
+﻿module ArmstrongNumbers
+
+let isArmstrongNumber number =
+    let charToNumber c = int c - int '0'
+    let numberAsString = string number
+    let charToPower c = pown (charToNumber c) numberAsString.Length
+    Seq.sumBy charToPower numberAsString = number
+ 
