246a7f3b0f6b6c85da4b3396fe636569ca0d7bdb
Jorge Yañez <ofeth@hotmail.com>
2020-06-02 19:08:40 -0600

    Add grains solution

---
+++ fsharp/grains/Grains.fs
@@ -0,0 +1,18 @@
+﻿module Grains
+
+open System.Numerics
+
+let private reducer x y =
+    match x, y with
+    | Ok a, Ok b -> Ok (a + b)
+    | _ -> Error "Invalid input"
+
+let square (n:int) =
+    match n with
+    | n when n <= 0 || n > 64 -> Error "square must be between 1 and 64"
+    | _ ->  Ok (2I ** (n - 1) |> uint64)
+
+let total = 
+    [1 .. 64]
+    |> List.map square
+    |> List.reduce reducer
