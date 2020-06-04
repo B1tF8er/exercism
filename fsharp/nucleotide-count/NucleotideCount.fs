3c222eb562369c6942e3432e9b97199f43734355
Jorge Yañez <ofeth@hotmail.com>
2020-06-03 21:04:33 -0600

    Add nucleotide count solution

---
+++ fsharp/nucleotide-count/NucleotideCount.fs
@@ -0,0 +1,24 @@
+﻿module NucleotideCount
+
+let private validNucleotides =
+    ['A'; 'T'; 'C'; 'G']
+
+let private isValid nucleotide =
+    validNucleotides
+    |> List.contains nucleotide
+
+let private increment sum nextNucleotide nucleotide =
+    if nextNucleotide = nucleotide then sum + 1 else sum
+
+let count (nucleotide:char) (strand:string) = 
+    match isValid nucleotide with
+    | true  -> (0, strand) ||> Seq.fold (fun sum nextNucleotide -> increment sum nextNucleotide nucleotide) 
+    | false -> failwith "Invalid nucleotide"
+
+let nucleotideCounts strand = 
+    if String.forall isValid strand then
+        List.map (fun nucleotide -> (nucleotide, count nucleotide strand)) validNucleotides 
+        |> Map.ofSeq
+        |> Some
+    else    
+        None
