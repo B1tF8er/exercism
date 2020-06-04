4545422a0837246a6e87be8338ff7147d006960f
Jorge Yañez <ofeth@hotmail.com>
2020-06-03 21:11:32 -0600

    Add etl solution

---
+++ fsharp/etl/Etl.fs
@@ -0,0 +1,17 @@
+﻿module Etl
+
+open System
+
+let normalizeLetter letter =
+    Char.ToLowerInvariant(letter)
+
+let transformLetterWithScore score lettersWithScore letter = 
+    lettersWithScore
+    |> Map.add (normalizeLetter letter) score
+
+let transformScoreWithLetters lettersWithScore score letters = 
+    letters
+    |> List.fold (transformLetterWithScore score) lettersWithScore
+
+let transform scoresWithLetters: Map<char, int> = 
+    Map.fold transformScoreWithLetters Map.empty scoresWithLetters
