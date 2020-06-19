c4f15433b0b6ac5eea12119ec27feec6d6ef3b34
Jorge Yañez <ofeth@hotmail.com>
2020-06-19 09:13:05 -0600

    Add proverb solution

---
+++ fsharp/proverb/Proverb.fs
@@ -0,0 +1,17 @@
+﻿module Proverb
+
+let line (want, lost) =
+    sprintf "For want of a %s the %s was lost." want lost
+
+let ending input =
+    sprintf "And all for the want of a %s." (input |> List.head)
+
+let recite input =
+    match input with
+    | [] -> []
+    | _ ->
+        let lines =
+            input
+            |> List.pairwise
+            |> List.map line
+        [ending input] |> List.append lines 
