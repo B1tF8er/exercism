7e4c545523e495c3279a6b069b064b4e113e1fe9
Jorge Yañez <ofeth@hotmail.com>
2020-06-22 18:09:02 -0600

    Add sieve solution

---
+++ fsharp/sieve/Sieve.fs
@@ -0,0 +1,10 @@
+﻿module Sieve
+
+let rec private sieve remainder primes =
+    match remainder with
+    | [] -> primes |> List.rev
+    | head::tail -> sieve (tail |> List.filter (fun x -> x % head > 0)) (head :: primes)
+    
+let primes limit = 
+    let possiblePrimes = [2 .. limit]    
+    sieve possiblePrimes []
