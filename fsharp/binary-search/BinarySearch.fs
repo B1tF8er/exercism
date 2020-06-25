22dfedac654bc532379172992a1862c96e3cb356
Jorge Yañez <ofeth@hotmail.com>
2020-06-25 15:42:31 -0600

    Add binary search solution

--- fsharp/binary-search/BinarySearch.fs
+++ fsharp/binary-search/BinarySearch.fs
@@ -10,13 +10,18 @@ let rec private binarySearch index value =
 
         match value with
         | value when value < middleValue ->
-            input.[0 .. middle - 1] |> binarySearch index value
+            input.[0 .. middle - 1]
+            |> binarySearch index value
         | value when value > middleValue ->
-            input.[middle + 1 ..] |> binarySearch (index + middle + 1) value 
+            input.[middle + 1 ..]
+            |> binarySearch (index + middle + 1) value 
         | _ ->
             Some (index + middle)
 
 let find input value =
     match (input |> Array.sort) <> input with
-    | true -> failwith "The input must be an ordered lists"
-    | false -> input |> binarySearch 0 value
+    | true ->
+        failwith "The input must be an ordered lists"
+    | false ->
+        input
+        |> binarySearch 0 value
