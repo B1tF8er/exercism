813e276b88e4459285e673a9ddbc0dd72cec797e
Jorge Yañez <ofeth@hotmail.com>
2020-06-25 15:49:50 -0600

    Add secret handshake solution

---
+++ fsharp/secret-handshake/SecretHandshake.fs
@@ -0,0 +1,18 @@
+﻿module SecretHandshake
+
+let commandsMapping = 
+    [
+        (1 <<< 0, (fun accumulator -> accumulator @ ["wink"]));
+        (1 <<< 1, (fun accumulator -> accumulator @ ["double blink"]));
+        (1 <<< 2, (fun accumulator -> accumulator @ ["close your eyes"]));
+        (1 <<< 3, (fun accumulator -> accumulator @ ["jump"]))
+        (1 <<< 4, (fun accumulator -> accumulator |> List.rev))
+    ]
+
+let applyCommand number accumulator (mask, apply) =
+    if number &&& mask <> 0 then apply accumulator
+    else accumulator
+
+let commands number =
+    ([], commandsMapping)
+    ||> List.fold (applyCommand number)
