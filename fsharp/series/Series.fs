8091a22e7295624e4e3bf8cd7b2d84f864d9d0b5
Jorge Yañez <ofeth@hotmail.com>
2020-07-01 21:06:22 -0600

    Add series solution

---
+++ fsharp/series/Series.fs
@@ -0,0 +1,10 @@
+﻿module Series
+
+let slices (value : string) (length : int) =
+    match (value, length) with
+    | _, length when length < 1 -> None
+    | value, length when length > value.Length -> None
+    | value, _  when value.Length = 0 -> None
+    | _ ->
+        let range = [0 .. value.Length - length]
+        Some [for i in range -> value.[i..i + length - 1]]
