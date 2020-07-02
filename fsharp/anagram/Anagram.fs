7c3c2292150f5e05c0ded401188cc4378e3ebbca
Jorge Yañez <ofeth@hotmail.com>
2020-07-01 20:53:54 -0600

    Add anagram solution

---
+++ fsharp/anagram/Anagram.fs
@@ -0,0 +1,23 @@
+﻿module Anagram
+
+open System
+
+let normalize (target : string) =
+    let valueNormalized =
+        target.ToLowerInvariant().ToCharArray()
+        |> Array.sort
+
+    new string(valueNormalized)
+
+let unequal target other =
+    let same =
+        String.Equals(target, other, StringComparison.InvariantCultureIgnoreCase)
+    
+    not (same)
+       
+let findAnagrams sources target = 
+    let isMatch source =
+        (source |> normalize) = (target |> normalize) && target |> unequal source
+    
+    sources
+    |> List.filter isMatch
