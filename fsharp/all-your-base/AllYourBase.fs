0d9d8248db70a51b8d4ae1e9952d56b833950b67
Jorge Yañez <ofeth@hotmail.com>
2020-07-03 16:05:44 -0600

    All your base

---
+++ fsharp/all-your-base/AllYourBase.fs
@@ -0,0 +1,34 @@
+﻿module AllYourBase
+
+let toBase b n =
+    let rec loop n acc =
+        match n with
+        | 0 -> acc
+        | _ -> 
+            let digit, n' = n % b, n / b
+            loop n' (digit::acc)
+
+    match loop n [] with
+    | [] -> Some [0]
+    | digits -> Some digits
+
+let fromBase b nums =
+    let rec loop acc = function
+    | [] -> Some acc
+    | digit::rest ->
+        match digit with
+        | digit when digit <  0 -> None
+        | digit when digit >= b -> None 
+        | _ -> loop (acc * b + digit) rest
+
+    loop 0 nums
+
+let rebase digits inputBase outputBase =
+    match inputBase < 2 || outputBase < 2 with
+    | true ->
+        None
+    | false ->
+        digits
+        |> List.skipWhile ((=) 0)
+        |> fromBase inputBase
+        |> Option.bind (toBase outputBase)
