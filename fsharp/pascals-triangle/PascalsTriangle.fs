3d29bc3daa2257f631505fbad55eca450bc1315e
Jorge Yañez <ofeth@hotmail.com>
2020-07-08 09:19:29 -0600

    Add pascals triangle solution

---
+++ fsharp/pascals-triangle/PascalsTriangle.fs
@@ -0,0 +1,12 @@
+﻿module PascalsTriangle
+
+let rows numberOfRows : int list list =
+    match numberOfRows with 
+    | rows when rows < 0 -> []
+    | _ ->
+        let row column = 
+            [ 1 .. column - 1 ] 
+            |> List.scan (fun acc j -> acc * (column - j) / j) 1 
+
+        [ 1 .. numberOfRows ] 
+        |> List.map row
