e234a815af66cb865b0dafe44ecd259b381cb8c1
Jorge Yañez <ofeth@hotmail.com>
2020-07-10 12:09:25 -0600

    Add dot dsl solution

---
+++ fsharp/dot-dsl/DotDsl.fs
@@ -0,0 +1,50 @@
+﻿module DotDsl
+
+type Attribute = string * string
+
+type Element =
+    | Attr of Attribute
+    | Node of string * Attribute list
+    | Edge of string * string * Attribute list
+
+type Graph = Element list
+
+let private isAttr element =
+    match element with
+    | Attr _ -> Some element
+    | _      -> None
+
+let private isNode element =
+    match element with
+    | Node _ -> Some element
+    | _      -> None
+
+let private isEdge element =
+    match element with
+    | Edge _ -> Some element
+    | _      -> None
+
+let graph children =
+    children
+    |> List.sort
+
+let attr key value =
+    Attr (key, value)
+
+let node key attrs =
+    Node (key, attrs)
+
+let edge left right attrs =
+    Edge (left, right, attrs)
+
+let attrs graph =
+    graph
+    |> List.choose isAttr
+
+let nodes graph =
+    graph
+    |> List.choose isNode
+
+let edges graph =
+    graph
+    |> List.choose isEdge
