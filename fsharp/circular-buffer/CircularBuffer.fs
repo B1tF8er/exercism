7dd529cafc0e5a655cda0377dd37a34bc080feec
Jorge Yañez <ofeth@hotmail.com>
2020-08-06 11:25:53 -0600

    Add circular buffer solution

---
+++ fsharp/circular-buffer/CircularBuffer.fs
@@ -0,0 +1,31 @@
+﻿module CircularBuffer
+
+type CircularBuffer<'a> = 
+    {
+        items: 'a list;
+        size: int 
+    }
+
+let mkCircularBuffer size =
+    { 
+        items = []
+        size = size 
+    }
+
+let clear buffer =
+    { buffer with items = [] }    
+
+let write value buffer = 
+    match List.length buffer.items = buffer.size with
+    | true -> failwith "Cannot write to full buffer"
+    | false ->{ buffer with items = buffer.items @ [value] }
+        
+let forceWrite value buffer =
+    match List.length buffer.items = buffer.size with
+    | true -> { buffer with items = List.tail buffer.items @ [value] }
+    | false  -> { buffer with items = buffer.items @ [value] }
+
+let read buffer =
+    match buffer.items with    
+    | x::xs -> x, { buffer with items = xs }
+    | [] -> failwith "Cannot read from empty buffer" 
