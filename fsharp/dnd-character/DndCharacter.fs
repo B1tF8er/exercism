95138e7c1bd9240a41e70ec2ba0c8ace7c10c783
Jorge Yañez <ofeth@hotmail.com>
2020-11-05 08:24:11 -0700

    Add dnd-character solution

---
+++ fsharp/dnd-character/DndCharacter.fs
@@ -0,0 +1,37 @@
+﻿module DndCharacter
+
+open System
+
+type character = 
+    { 
+        Strength: int
+        Dexterity: int
+        Constitution: int
+        Intelligence: int
+        Wisdom: int
+        Charisma: int
+        Hitpoints: int
+    }
+
+let modifier x =
+    floor (float(x - 10) / 2.) |> int
+
+let ability() = 
+    [1 .. 4]
+    |> List.map (fun _ -> Random().Next(1, 7))
+    |> List.sortDescending
+    |> Seq.take 3
+    |> Seq.sum
+
+let createCharacter() : character =
+    let constitution = ability()
+
+    {
+        Strength = ability()
+        Dexterity = ability()
+        Constitution = constitution
+        Intelligence = ability()
+        Wisdom = ability()
+        Charisma = ability()
+        Hitpoints = 10 + (modifier constitution)
+    }
